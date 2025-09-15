fc0c0bda0dce8f4c8299c81b71028e8984c4efa0
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-09-15 23:56:30 +0530

    configure environment variables and establish database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+connectDB();
 
-app.listen(5001, () => {
-  console.log("Server running on port 5001");
+app.get("/", (req, res) => {
+  res.status(200).send("Hello from the backend!");
+});
+
+app.listen(ENV.PORT, () => {
+  console.log("Server running on port", ENV.PORT);
 });
